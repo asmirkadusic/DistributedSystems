3047add07dd52ada50ccba717caf5a00f7268d18
asmir <Asmir Kadusic>
2022-11-22 00:48:34 +0100

    2022/11/19, Fix gitignore file, fix routing problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new"
-  post "users", to: "registrations#create"
+  post "sign_up", to: "registrations#create"
 
   get "/", to: "main#index"
 end
