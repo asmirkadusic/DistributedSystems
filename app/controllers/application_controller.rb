0f8aaea3b76f4076b3a57380b7cc3122a524d8a8
asmir <Asmir Kadusic>
2022-12-12 01:37:35 +0100

    2022/12/11, Small changes related to model and database migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :set_current_user
-    
+    protect_from_forgery with: :null_session    
     def set_current_user
         if session[:user_id]
             Current.user = User.find_by(id: session[:user_id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  attr_accessor :email, :password
 end
